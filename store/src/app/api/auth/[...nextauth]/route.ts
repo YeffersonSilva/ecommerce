1a4d4953d9f18379af47eea15e7421e9fd8d9e42
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-07-25 22:54:48 -0300

    chore: Add user tables to the database schema

--- store/src/app/api/auth/[...nextauth]/route.ts
+++ store/src/app/api/auth/[...nextauth]/route.ts
@@ -3,15 +3,7 @@ import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import { prismaClient } from "@/lib/pisma"
+import { authOptions } from "@/lib/auth"
 
 
-export default NextAuth({
-    adapter: PrismaAdapter(prismaClient),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  
-})
+export default NextAuth(authOptions)
